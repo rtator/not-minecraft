93e1fabad5a6313e156160c776a9f5b9319eda7c
rtator <66912559+rtator@users.noreply.github.com>
2020-07-21 14:32:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,236 @@
+namespace SpriteKind {
+    export const Resource = SpriteKind.create()
+    export const HUD = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+. . . . . . . . 1 2 . . . . . . 
+. . . . . . . . 1 2 2 2 . . . . 
+. . . . . . . . 1 2 2 2 2 . . . 
+. . . . . . . . 1 2 2 2 . . . . 
+. . . . . . . . 1 2 . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . d d d d d d d . . . . 
+. . . . d d d d d d d d d . . . 
+. . . d d d d d d d d d d d . . 
+. . . d d d d d d d d d d d . . 
+. . . d d d d d d d d d d d . . 
+`
+}
+/**
+ * Ice - 0
+ * 
+ * Rock - 1
+ * 
+ * Algae - 2
+ */
+scene.onHitWall(SpriteKind.Player, function (sprite) {
+    if (controller.A.isPressed()) {
+        if (controller.left.isPressed()) {
+            for (let direction of [CollisionDirection.Left]) {
+                breakTileInDirection(direction)
+            }
+        } else if (controller.right.isPressed()) {
+            for (let direction of [CollisionDirection.Right]) {
+                breakTileInDirection(direction)
+            }
+        } else if (controller.up.isPressed()) {
+            for (let direction of [CollisionDirection.Top]) {
+                breakTileInDirection(direction)
+            }
+        } else if (controller.down.isPressed()) {
+            for (let direction of [CollisionDirection.Bottom]) {
+                breakTileInDirection(direction)
+            }
+        }
+    }
+})
+function breakTileInDirection (direction: number) {
+    locationOfWall = tilemap.locationInDirection(tilemap.locationOfSprite(mySprite), direction)
+    if (tilemap.tileIsWall(locationOfWall) && !(tilemap.tileIs(locationOfWall, myTiles.tile5)) && !(tilemap.tileIs(locationOfWall, myTiles.tile2)) && !(tilemap.tileIs(locationOfWall, myTiles.tile1))) {
+        tiles.setWallAt(locationOfWall, false)
+        tiles.setTileAt(locationOfWall, myTiles.tile0)
+    }
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -110
+    }
+})
+let locationOfWall: tiles.Location = null
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002020200000000000000000000000000020202000000000000000000000000000003000000000000000000000000000000030000000000000000040000030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030505050505030303030303030303030305050505050303030303030303030303050505050503030303030303030303030505050505`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 . . . . . . . . . . 
+. . . 2 2 2 . . . . . . . . . . 
+. . . . 2 . . . . . . . . . . . 
+. . . . 2 . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6],
+            TileScale.Sixteen
+        ))
+scene.setBackgroundColor(9)
+let maxNumberOfResource = 10
+mySprite = sprites.create(img`
+. . . . 5 f 5 5 5 f 5 . . . . 
+. . 5 5 5 f 5 5 5 5 5 5 5 . . 
+. 5 5 5 5 f f f 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+f 5 5 5 5 5 f 5 f 5 5 5 5 5 f 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 f 5 f 5 5 5 5 5 f 5 f 5 5 
+5 5 f 5 f 5 5 5 5 5 f 5 f 5 5 
+f f f 5 f f f f f f f 5 f f f 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 f f f 5 5 5 5 5 5 . 
+. . 5 5 5 f 5 5 5 5 5 5 5 . . 
+. . . . 5 f 5 5 5 f 5 . . . . 
+`, SpriteKind.Player)
+mySprite.ay = 200
+mySprite.z = 10
+controller.moveSprite(mySprite, 100, 0)
+tiles.placeOnRandomTile(mySprite, myTiles.tile4)
+scene.cameraFollowSprite(mySprite)
+let resourceTiles = [myTiles.tile3, myTiles.tile1, myTiles.tile2]
+let auspiciousInventory: number[] = []
+let storedInventory: number[] = []
+let resourceHUDBars: number[] = []
+for (let index = 0; index <= resourceTiles.length - 1; index++) {
+    auspiciousInventory.push(0)
+    storedInventory.push(0)
+}
